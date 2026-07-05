96cc3bc252ecd0457b5ac93cfe1305fb045656a2
Gian Ignacio Rodriguez <gian.rodriguez207@gmail.com>
2026-07-05 14:12:13 -0300

    Menú
    Se añade un botón de menú desplegable para mejora visual del navbar de admin

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use(session({
 
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 app.use('/admin.css', express.static(path.join(__dirname, 'public/admin.css')));
+app.use('/menu-icon.png', express.static(path.join(__dirname, 'public/menu-icon.png')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src/views'));
