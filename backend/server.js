c7ccc6597faea62ba12f87922537418c5b6447da
Gian Ignacio Rodriguez <gian.rodriguez207@gmail.com>
2026-07-07 17:21:56 -0300

    Seed Productos
    Se realiza un seed de los productos para testear

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 import app from './app.js';
 import { syncDB } from './src/models/index.js';
 import Usuario from './src/models/Usuario.js';
+import { seedProductos } from "./src/infraestructura/seedProductos.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -30,6 +31,7 @@ const crearUsuarioAdmin = async () => {
 
 syncDB().then(async () => {
   await crearUsuarioAdmin();
+  await seedProductos();
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
     console.log(`API: http://localhost:${PORT}/api/productos`);
